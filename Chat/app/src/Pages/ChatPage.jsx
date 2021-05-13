bc96728f8d681c29f848d29a280bb906f31f77ff
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-05-13 12:24:34 +0200

    works functionally

--- Chat/app/src/Pages/ChatPage.jsx
+++ Chat/app/src/Pages/ChatPage.jsx
@@ -1,24 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import socketClient from "socket.io-client";
 
 function ChatPage({ userName }) {
+    const [chat, setChat] = useState([]);
+    const [input, setInput] = useState("");
+    const socket = socketClient("ws://localhost:8080");
+
     useEffect(() => {
         getOldMessages();
         connectToChat();
     }, []);
 
     const connectToChat = () => {
-        const socket = socketClient("ws://localhost:8080");
         socket.on("message", data => {
+            console.log("This is the pres Chat: " + chat);
             console.log(data);
+            setChat(chat => [...chat, data]);
         })
     }
     const getOldMessages = () => {
-        
+        fetch("http://localhost:8080/getAllMessages")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setChat(data);
+            });
+    }
+    const sendMessage = () => {
+        socket.emit("message", { name: userName, message: input });
     }
     return (
         <div>
             <h1>Chat</h1>
+            <ul>
+                {chat.map((item, key) => (
+                    <div key={key}>
+                        <p>{item.name}</p>
+                        <h3>{item.message}</h3>
+                    </div>
+                ))}
+            </ul>
+            <input type="text" name="" id="" onChange={(e) => { setInput(e.target.value) }} />
+            <button onClick={sendMessage}>Send</button>
         </div>
     )
 }
--- Chat/server/index.js
+++ Chat/server/index.js
@@ -1,43 +1,29 @@
-//We get Express and Create an Express App
 const express = require('express');
 const app = express();
-//We get HTTP and Cros to use both with Express and Socket.io
 const http = require('http');
 var cors = require('cors')
-//We create the Server
 const server = http.createServer(app);
-//We enable Cors
 app.use(cors())
 const io = require("socket.io")(server,{
     cors:{origin:"*"}
 });
-
-
-//We set Express to Format in JSON
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.status(200).send({
-        name:`Server`,
-        message:`Hello G`
-    })
-});
+const chat = [];
 
-io.on('connection',(socket)=>{  
-    console.log("a user connected");
-
-    socket.on('message',(message)=>{
-        console.log(message);
-        io.emit("message",`${message}`);
-    });
-
-    socket.on("smallchat",(message)=>{
-        console.log(message);
-        io.emit("smallchat",`New message: ${message}`);
-    })
+app.get("/getAllMessages",(req,res)=>{
+    res.status(200).send(chat);
 })
 
-//We start the Server
+
+io.on('connection',(socket)=>{  
+    socket.on('message',(message)=>{
+        console.log(message);
+        chat.push(message);
+        io.emit("message",message);
+    });
+})
+
 server.listen(8080, () => {
   console.log('listening on *:8080');
 });
